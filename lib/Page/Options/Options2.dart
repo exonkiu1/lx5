1b466aa504b3fc9b35303afebdf3eb18b0ac0cf7
exonkiu1 <ehsankiumarsi7@gmail.com>
2025-06-30 18:50:41 +0330

    test12

--- lib/Page/Options/Options2.dart
+++ lib/Page/Options/Options2.dart
@@ -48,42 +48,33 @@ class _Options2State extends State<Options2> {
               children: List.generate(
                 //  Get.find<Controlleroptions>().lenght.value,
                 PageModel[Get.find<Controllerinfo>().Model.value]!.length,
-                (i) => Obx(() {
-                  return Opacity(
-                    opacity:
-                        Get.find<Controlleroptions>().OpacityOption[i].value >=
-                                0.7
-                            ? 1.0
-                            : Get.find<Controlleroptions>().OpacityOption[i].value,
-                    child: InkWell(
-                        onTap: () => Get.to(
-                              PageModel[Get.find<Controllerinfo>().Model.value]!
-                                  .values
-                                  .elementAt(i)['page'],
-                            ),
-                        child: Container(
-                          width: Get.width * 0.3,
-                          margin: EdgeInsets.symmetric(vertical: 15),
-                          child: Center(
-                            child: Column(
-                              children: [
-                                Obx(() {
-                                  return Image.asset(
-                                    'assets/image/options2/${PageModel[Get.find<Controllerinfo>().Model.value]!.values.elementAt(i)['image']}.png',
-                                    width: Get.width * 0.2,
-                                  );
-                                }),
-                                Obx(() {
-                                  return Text(
-                                    '${PageModel[Get.find<Controllerinfo>().Model.value]!.keys.elementAt(i)}',
-                                  );
-                                }),
-                              ],
-                            ),
+                (i) =>  InkWell(
+                      onTap: () => Get.to(
+                            PageModel[Get.find<Controllerinfo>().Model.value]!
+                                .values
+                                .elementAt(i)['page'],
                           ),
-                        )),
-                  );
-                }),
+                      child: Container(
+                        width: Get.width * 0.3,
+                        margin: EdgeInsets.symmetric(vertical: 15),
+                        child: Center(
+                          child: Column(
+                            children: [
+                              Obx(() {
+                                return Image.asset(
+                                  'assets/image/options2/${PageModel[Get.find<Controllerinfo>().Model.value]!.values.elementAt(i)['image']}.png',
+                                  width: Get.width * 0.2,
+                                );
+                              }),
+                              Obx(() {
+                                return Text(
+                                  '${PageModel[Get.find<Controllerinfo>().Model.value]!.keys.elementAt(i)}',
+                                );
+                              }),
+                            ],
+                          ),
+                        ),
+                      ))
               ),
             );
           }),
