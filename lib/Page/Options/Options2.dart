c56ca9c4cda9918f663edfe92e46ab579baa10ef
exonkiu1 <ehsankiumarsi7@gmail.com>
2025-06-30 18:23:28 +0330

    test10

--- lib/Page/Options/Options2.dart
+++ lib/Page/Options/Options2.dart
@@ -27,14 +27,12 @@ class _Options2State extends State<Options2> {
   @override
   void initState() {
     Get.find<Controlleroptions>().StartShowing();
-    // TODO: implement initState
     super.initState();
   }
 
   @override
   void dispose() {
     Get.find<Controlleroptions>().DisposeOpacity();
-    // TODO: implement dispose
     super.dispose();
   }
 
@@ -50,45 +48,42 @@ class _Options2State extends State<Options2> {
               children: List.generate(
                 //  Get.find<Controlleroptions>().lenght.value,
                 PageModel[Get.find<Controllerinfo>().Model.value]!.length,
-                (i) => Obx(
-                  () {
-                    return Opacity(
-                      opacity:
-                          Get.find<Controlleroptions>().OpacityOption[i].value >= 1
-                              ? 1
-                              : Get.find<Controlleroptions>()
-                                  .OpacityOption[i]
-                                  .value,
-                      child: InkWell(
-                          onTap: () => Get.to(
-                                PageModel[Get.find<Controllerinfo>().Model.value]!
-                                    .values
-                                    .elementAt(i)['page'],
-                              ),
-                          child: Container(
-                            width: Get.width * 0.3,
-                            margin: EdgeInsets.symmetric(vertical: 15),
-                            child: Center(
-                              child: Column(
-                                children: [
-                                  Obx(() {
-                                    return Image.asset(
-                                      'assets/image/options2/${PageModel[Get.find<Controllerinfo>().Model.value]!.values.elementAt(i)['image']}.png',
-                                      width: Get.width * 0.2,
-                                    );
-                                  }),
-                                  Obx(() {
-                                    return Text(
-                                      '${PageModel[Get.find<Controllerinfo>().Model.value]!.keys.elementAt(i)}',
-                                    );
-                                  }),
-                                ],
-                              ),
+                (i) => Obx(() {
+                  return Opacity(
+                    opacity:
+                        Get.find<Controlleroptions>().OpacityOption[i].value >=
+                                0.7
+                            ? 1
+                            : Get.find<Controlleroptions>().OpacityOption[i].value,
+                    child: InkWell(
+                        onTap: () => Get.to(
+                              PageModel[Get.find<Controllerinfo>().Model.value]!
+                                  .values
+                                  .elementAt(i)['page'],
                             ),
-                          )),
-                    );
-                  }
-                ),
+                        child: Container(
+                          width: Get.width * 0.3,
+                          margin: EdgeInsets.symmetric(vertical: 15),
+                          child: Center(
+                            child: Column(
+                              children: [
+                                Obx(() {
+                                  return Image.asset(
+                                    'assets/image/options2/${PageModel[Get.find<Controllerinfo>().Model.value]!.values.elementAt(i)['image']}.png',
+                                    width: Get.width * 0.2,
+                                  );
+                                }),
+                                Obx(() {
+                                  return Text(
+                                    '${PageModel[Get.find<Controllerinfo>().Model.value]!.keys.elementAt(i)}',
+                                  );
+                                }),
+                              ],
+                            ),
+                          ),
+                        )),
+                  );
+                }),
               ),
             );
           }),
