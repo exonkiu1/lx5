e8b21c32a077fe0bb62cd22a9e684ec6b48b9815
ehsan <ehsankiumarsi7@gmail.com>
2025-07-12 20:20:11 +0330

    test 1 for build apk nd add supabase

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,16 @@ import 'package:get/get.dart';
 import 'package:lx/Getx/binding.dart';
 import 'package:lx/WidgetUi/BackGroundView.dart';
 import 'package:lx/WidgetUi/decoration.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
+void main() async{
+   WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
+  await Supabase.initialize(
+    url: 'https://swrgytwvmqthgtwaqpaa.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3cmd5dHd2bXF0aGd0d2FxcGFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk2NTI5MjIsImV4cCI6MjA2NTIyODkyMn0.se6M3E0UgwlOIUDgSll5KOZGKdtlU1LZfUNvYbKNhmY',
+  );
   runApp(const MyApp());
 }
 
