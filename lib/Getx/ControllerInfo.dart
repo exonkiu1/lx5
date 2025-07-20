368515a382460f36ccb7ee15825e7384a28bce4d
ehsan <ehsankiumarsi7@gmail.com>
2025-07-20 19:02:18 +0330

    edit7

--- lib/Getx/ControllerInfo.dart
+++ lib/Getx/ControllerInfo.dart
@@ -81,8 +81,8 @@ class Controllerinfo extends GetxController {
 
 Map<String, String> codeInquiryCharge = {
   'ha': '*140*11',
-  'ir': '*555*1*2',
-  'rl': '*200*2*2*1'
+  'ir': '*555*1*2*2',
+  'rl': '*140'
 };
 List<String> ha = [
   '0910',
