b58cd1d6cedf0872933b8eb201519f98227e2cba
ehsan <ehsankiumarsi7@gmail.com>
2025-10-06 19:54:07 +0330

    Fubu

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:lx/Getx/ControllerHomePage.dart';
 import 'package:lx/Getx/binding.dart';
 import 'package:lx/WidgetUi/BackGroundView.dart';
 import 'package:lx/WidgetUi/decoration.dart';
-String NameDev = 'SignalAlarm';
+String NameDev = 'Fubu';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
