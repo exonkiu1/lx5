c190e5bcfcf00cbef572a16738dacf1d778289b7
ehsan <ehsankiumarsi7@gmail.com>
2025-10-06 18:49:05 +0330

    v11

--- lib/Page/SplashScreen.dart/SplashScreen.dart
+++ lib/Page/SplashScreen.dart/SplashScreen.dart
@@ -27,7 +27,7 @@ class _SplashscreenState extends State<Splashscreen> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [const Text(NameDev, style: TextStyle(fontSize: 40))],
+          children: [ Text('SignalAlarm', style: TextStyle(fontSize: 40))],
         ),
       ),
     );
