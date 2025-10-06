ade2d919205ff8bc3ebb29cd0cb837f5ebe73652
ehsan <ehsankiumarsi7@gmail.com>
2025-10-06 19:55:22 +0330

    Zetron

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:lx/Getx/ControllerHomePage.dart';
 import 'package:lx/Getx/binding.dart';
 import 'package:lx/WidgetUi/BackGroundView.dart';
 import 'package:lx/WidgetUi/decoration.dart';
-String NameDev = 'Fubu';
+String NameDev = 'Zetron';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
