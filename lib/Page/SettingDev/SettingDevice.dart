c21826e8f5beccf0f61fbb34602969c4dcfac43a
exonkiumarsi <ehsankiumarsi7@gmail.com>
2025-10-09 18:39:57 +0330

    v1 melody lx

--- lib/Page/SettingDev/SettingDevice.dart
+++ lib/Page/SettingDev/SettingDevice.dart
@@ -53,7 +53,7 @@ class WidgetMelodyAlarm extends StatelessWidget {
     return Obx(
       () {
         return Visibility(
-          visible: Get.find<Controllerinfo>().Model.value == 'LX PRO',
+          visible: Get.find<Controllerinfo>().Model.value != 'LX PRO',
           child: Container(
             width: Get.width * 0.9,
             decoration: decoration(),
